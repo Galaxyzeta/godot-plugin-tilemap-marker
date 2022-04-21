bb79f1da62a8c121e67c06ffd35598bf86756741
Galaxyzeta <1919737818@qq.com>
2022-04-21 16:35:23 +0800

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# Tilemap Marker
+
+A Godot game engine plugin that marks current selected tilemap's cells and boundaries.
+
+I made this because there's some invisible autotiles in my scene that is no where to be found.
+
+![Illustration](../tilemap_marker/gif/Illustration.gif)
+
+# Installation
+
+You may refer to the official installation documentation here:
+https://docs.godotengine.org/en/stable/tutorials/plugins/editor/installing_plugins.html#installing-a-plugin
+
+# Usage
+
+1. Create a tilemap node in the scene.
+2. Draw something on the tilemap and a red area with boundary will occur.
+3. Click toggle to disable the drawing.
+4. Will not draw when the current node is not a tilemap.
---
+++ plugin.gd
@@ -0,0 +1,76 @@
+tool
+extends EditorPlugin
+
+var _selection: EditorSelection
+var _active_tilemap: TileMap
+var _color: Color = Color.red
+var _button_scene: PackedScene = preload("./button.tscn")
+var _button_instance: Button		# Lazy instanced
+
+var _enable_drawing: bool = true
+
+signal __selection_ok
+
+func _enter_tree():
+	# Load button preset
+	if _button_scene == null:
+		disable_plugin()
+		return
+	_selection = get_editor_interface().get__selection()
+	_selection.connect("_selection_changed", self, "_on__selection_changed")
+
+func _exit_tree():
+	if _button_instance != null:
+		_button_instance.queue_free()
+
+func _on_button_pressed():
+	_enable_drawing = not _enable_drawing
+	update_overlays()
+
+func _on__selection_changed():
+	# Returns an array of selected nodes
+	var selected = _selection.get_selected_nodes()
+	var switch = false
+	if not selected.empty():
+		for n in selected:
+			if n is TileMap:
+				_active_tilemap = n
+				switch = true
+				break
+	if not switch:
+		_active_tilemap = null
+	emit_signal("__selection_ok")
+
+func handles(obj: Object)->bool:
+	yield(self, "__selection_ok")
+	return obj == _active_tilemap
+
+func forward_canvas_draw_over_viewport(overlay: Control) -> void:
+	# Handle button lazy instantiation
+	if _button_instance == null:
+		_button_instance = _button_scene.instance()
+		_button_instance.connect("button_down", self, "_on_button_pressed")
+	if _button_instance != null && _button_instance.get_parent() != null:
+		_button_instance.get_parent().remove_child(_button_instance)
+	if _active_tilemap == null or not _active_tilemap.is_inside_tree():
+		return
+	var mat: Transform2D = _active_tilemap.get_viewport_transform() * overlay.get_canvas_transform()
+	var sc: Vector2 = mat.get_scale()
+	# Draw boundary
+	var r2 = _active_tilemap.get_used_rect()
+	var pos = mat * (_active_tilemap.global_position + r2.position * _active_tilemap.cell_size)
+	var wh = sc * r2.size * _active_tilemap.cell_size
+	var ending = pos + wh
+	if _enable_drawing:
+		_color.a = 1
+		overlay.draw_rect(Rect2(pos, wh), _color, false, 2.0)
+		# Draw grids
+		_color.a = 0.3
+		for vec2 in _active_tilemap.get_used_cells():
+			var p = mat * (_active_tilemap.global_position + vec2 * _active_tilemap.cell_size)
+			overlay.draw_rect(Rect2(p, sc * _active_tilemap.cell_size), _color, true)
+		# Draw size info
+		overlay.draw_string(overlay.get_font(""), ending, str(r2.size))
+	# Add a button to the vp
+	overlay.add_child(_button_instance)
+	_button_instance.set_position(ending + Vector2(0, 8))
